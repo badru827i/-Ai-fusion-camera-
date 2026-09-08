7ae0b9bf455c16bbefaa28846455ad890a9cf34a
badru827i <badrulmunir816@gmail.com>
2026-09-08 18:44:00 +0800

    Bump app version for update system

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.badru.aifusioncamera"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0.0"
+        versionCode = 4
+        versionName = "3.0.1"
     }
 
     buildTypes {
